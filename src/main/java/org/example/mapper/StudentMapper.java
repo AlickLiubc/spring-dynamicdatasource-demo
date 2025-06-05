97c225ea7926d72900ecac5aa44ccfc75626c4b4
AlickLiubc <nrlbc@sina.com>
2025-06-05 16:42:44 +0800

    添加批量新增数据的DAO层代码

--- src/main/java/org/example/mapper/StudentMapper.java
+++ src/main/java/org/example/mapper/StudentMapper.java
@@ -4,11 +4,15 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.example.entity.Student;
 
+import java.util.List;
+
 @Mapper
 public interface StudentMapper {
 
     Student selectOne(Integer id);
 
     int insertStudent(Student student);
+
+    int batchInsertStudent(@Param("itemList") List<Student> itemList);
 
 }
